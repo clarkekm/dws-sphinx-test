15522f9f44db55f8ff3c3d7d0343e7283351fb0f
clarkekm <80916073+clarkekm@users.noreply.github.com>
2024-05-01 15:03:20 +0100

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -17,10 +17,14 @@ release = '0.0.1'
 templates_path = ['_templates']
 exclude_patterns = []
 
-
-
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = "sphinx_rtd_theme"
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ["_static"]
